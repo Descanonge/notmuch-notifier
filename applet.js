ae4a5e505158d9db18d6e640fbf2ac3e17c96772
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2021-12-17 16:38:49 +0100

    Add archive/trash/delete buttons

--- applet.js
+++ applet.js
@@ -13,6 +13,20 @@ function MailItem(mail) {
     this._init(mail);
 }
 
+function run_cmd(command) {
+    try {
+        let [result, stdout, stderr] = GLib.spawn_command_line_sync(command);
+        if (stdout != null) {
+            return stdout.toString();
+        }
+    }
+    catch(e) {
+        global.logError(e);
+    }
+
+    return "";
+}
+
 MailItem.prototype = {
     __proto__: PopupMenu.PopupBaseMenuItem.prototype,
 
@@ -20,22 +34,55 @@ MailItem.prototype = {
         PopupMenu.PopupBaseMenuItem.prototype._init.call(this);
 
         try {
+            this.id = mail.thread;
             this.subject = mail.subject;
             this.sender = mail.authors;
 
-            this._sender_label = new St.Label(
-                {text: this.sender, style_class: "notmuch-sender-label"});
-            this._subject_label = new St.Label(
-                {text: this.subject, style_class: "notmuch-subject-label"});
+            this._hBox = new St.BoxLayout({vertical: false});
+            this._label = new St.BoxLayout({vertical: true});
+            this._buttons = new St.BoxLayout({vertical: false});
 
-            this._vBox = new St.BoxLayout({vertical: true});
-            this._vBox.add(this._sender_label);
-            this._vBox.add(this._subject_label);
-            this.addActor(this._vBox, {expand: true});
+            this.make_label();
+            this.make_buttons();
+            this._hBox.add(this._label);
+            this._hBox.add(this._buttons);
+
+            this.addActor(this._hBox, {expand: true});
 
         } catch (e) {
             global.logError(e);
         }
+    },
+
+    make_label: function() {
+        this._sender_label = new St.Label(
+            {text: this.sender, style_class: "notmuch-sender-label"});
+        this._subject_label = new St.Label(
+            {text: this.subject, style_class: "notmuch-subject-label"});
+
+        this._label.add(this._sender_label);
+        this._label.add(this._subject_label);
+
+    },
+
+    _make_icon: function(name) {
+        let icon = new St.Icon({
+            icon_name: name,
+            icon_type: St.IconType.SYMBOLIC,
+            icon_size: 24,
+            style: "popup-menu-icon"
+        })
+        return icon;
+    },
+
+    make_buttons: function() {
+        this._btn_archive = new St.Button({child: this._make_icon("archive")});
+        this._btn_trash = new St.Button({child: this._make_icon("edit-clear-history")});
+        this._btn_delete = new St.Button({child: this._make_icon("edit-delete")});
+        let kw = {expand: false, align: St.Align.END, spacing: 12};
+        this._buttons.add(this._btn_archive, kw);
+        this._buttons.add(this._btn_trash, kw);
+        this._buttons.add(this._btn_delete, kw);
     },
 
     activate: function(event, keepMenu) {
@@ -60,7 +107,6 @@ NotmuchNotifier.prototype = {
             this.instance_id = instance_id;
 
             this.set_applet_icon_symbolic_name("mail-unread");
-            this.set_applet_tooltip(_("Mark mail as read"));
             this.set_applet_label("");
 
             this.settings = new Settings.AppletSettings(this, metadata.uuid, instance_id);
@@ -68,26 +114,13 @@ NotmuchNotifier.prototype = {
             this.settings.bind("max-mail-summary", "max_mail_summary", this.max_mail_summary);
 
             this.menuManager = new PopupMenu.PopupMenuManager(this);
-            this.menu = new Applet.AppletPopupMenu(this, orientation);
+            this.menu = new Applet.AppletPopupMenu(this, orientation, {sensitive: false});
             this.menuManager.addMenu(this.menu);
-            this.init_menu();
 
             this.mail_count = 0;
             this.mail_unread = false;
             this.mail_items = [];
             this._update_loop();
-        } catch(e) {
-            global.logError(e);
-        }
-    },
-
-    init_menu: function() {
-        try {
-            // First item: Force refresh mail now
-            let item = new PopupMenu.PopupIconMenuItem("Force refresh", "mail-message-new",
-                                                       St.IconType.FULLCOLOR);
-            item.connect('activate', Lang.bind(this, this.refresh));
-            this.menu.addMenuItem(item);
         } catch(e) {
             global.logError(e);
         }
@@ -105,7 +138,7 @@ NotmuchNotifier.prototype = {
             this.load_menu();
         }
         this.menu.toggle();
-        this._run_cmd("notmuch tag -unread tag:inbox and tag:unread");
+        run_cmd("notmuch tag -unread tag:inbox and tag:unread");
         this.update_label();
     },
 
@@ -123,19 +156,20 @@ NotmuchNotifier.prototype = {
     },
 
     refresh: function() {
-        this._run_cmd("mbsync -aq");
-        this._run_cmd("notmuch new");
+        run_cmd("notmuch new --no-hooks --quiet");
+        run_cmd("mbsync -aq");
+        run_cmd("notmuch new --quiet");
         this.update_label();
     },
 
     get_messages: function() {
-        let count = this._run_cmd("notmuch count tag:inbox");
-        this.mail_unread = (this._run_cmd("notmuch count tag:inbox and tag:unread") > 0);
+        let count = run_cmd("notmuch count tag:inbox");
+        this.mail_unread = (run_cmd("notmuch count tag:inbox and tag:unread") > 0);
         this.mail_count = count.replace( /[\r\n]+/gm, "" );
     },
 
     get_messages_summary: function() {
-        let mails_s = this._run_cmd(`notmuch search --format=json --limit=${this.max_mail_summary} tag:inbox`);
+        let mails_s = run_cmd(`notmuch search --format=json --limit=${this.max_mail_summary} tag:inbox`);
         if (mails_s != "") {
             try {
                 for (var mail of JSON.parse(mails_s)) {
@@ -162,24 +196,21 @@ NotmuchNotifier.prototype = {
         }
     },
 
-    _run_cmd: function(command) {
-        try {
-            let [result, stdout, stderr] = GLib.spawn_command_line_sync(command);
-            if (stdout != null) {
-                return stdout.toString();
-            }
-        }
-        catch(e) {
-            global.logError(e);
-        }
-
-        return "";
-    },
-
     _update_loop: function() {
         this.update_label();
         this._updateLoopID = Mainloop.timeout_add(this.update_interval*1000,
                                                   Lang.bind(this, this._update_loop));
+    },
+
+    bind_remove: function(btn, mi, tag) {
+        let func = function(){
+            run_cmd(`notmuch tag ${tag} -- thread:${mi.id}`);
+            this.load_menu();
+            this.menu.toggle();
+            // mi.destroy();
+            // this.update_label();
+        }
+        btn.connect('clicked', Lang.bind(this, func));
     },
 
     load_menu: function() {
@@ -195,9 +226,13 @@ NotmuchNotifier.prototype = {
             }
             count ++;
             this.menu.addMenuItem(mi);
+            this.bind_remove(mi._btn_archive, mi, '-inbox')
+            this.bind_remove(mi._btn_trash, mi, '+trashed')
+            this.bind_remove(mi._btn_delete, mi, '+deleted')
+
         }
         if (this.mail_count == 0) {
-            this._noUnreadItem = this.menu.addAction(_("No unread mails."));
+            this._noUnreadItem = this.menu.addAction(_("No mail in inbox."));
         }
     },
 
